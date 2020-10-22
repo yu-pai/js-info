0facdd24a2f981b3f018249bed8641943fed9c04
yu bai <bbaiyuby@gmail.com>
2020-10-22 21:50:54 +0200

    variables

--- my.js
+++ my.js
@@ -1,5 +1,30 @@
-alert( 'Hello, world!' );
+// alert( 'Hello, world!' );
+
+
 /*
     nested comment
 */
-[1, 2].forEach(alert)
+
+
+// [1, 2].forEach(alert)
+
+
+/* Variables */
+// let message = 'Hello!'
+// alert(message);
+
+// let user = 'John';
+// let age = 25;
+// let message = 'Hello';
+
+// let message;
+// message = 'Hello!';
+// message = 'World!';
+// alert(message);
+
+let hello = 'Hello world!';
+let message;
+// copy 'Hello world' from hello into message
+message = hello;
+alert(hello);
+alert(message);
